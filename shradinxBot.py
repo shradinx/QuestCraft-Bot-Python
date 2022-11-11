e7c85ee8bde2095ad558be8144f3d9d072dc4f48
shradinx <107893057+shradinx@users.noreply.github.com>
2022-11-11 12:22:52 -0500

    remove multi-line comment
    the multi-line comment at the top has been reused in the README.md file

--- shradinxBot.py
+++ shradinxBot.py
@@ -4,24 +4,6 @@
 # Interactions.py bot made for QuestCraft
 # ------------------------------------------------------------------------------
 
-"""
-- All commands ran by the user are equipped with a print statement giving the username and the command ran.
-
-NOTE: The token.txt file is required for this bot to work and must be in the same folder directory as the python file.
-- I will not include my personal token.txt file, however you can create one by following these steps below.
-    1. Download this python file
-    2. In the same folder as this file, create a new .txt document titled token.txt
-    3. Paste your token in the token.txt file you have created
-- If you get an error where the file cannot be found:
-    1. Make sure it is 100% in the same folder as your python file.
-    2. (OPTIONAL) Use the exact file path to the file *format below*
-        file = open(r"C:\Users\Example\Document\pythonBot\token.txt\")
-
-- Credits to the interactions.py Discord developers for advice on certain parts of the program.
-- Credits to Cartrigger for the original QuestCraft bot, written in JavaScript, along with *most*
-  of the commands and outputs.
-"""
-
 # Import all modules needed for bot.
 import interactions
 from interactions import autodefer
